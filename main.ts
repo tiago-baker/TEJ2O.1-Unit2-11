1f65846daa49e5918bece16c8be8e15148c93e9f
tiago-baker <tiago.baker@stu.ocsb.ca>
2025-10-24 13:16:29 -0400

    comments

--- main.ts
+++ main.ts
@@ -5,30 +5,34 @@
  * This program does an else if statment
 */
 
+// setup
 basic.clearScreen()
 basic.showIcon(IconNames.Happy)
 
-// varbles 
-let inerger1 : number = 0
-let inerger2 : number = 0
+// variables 
+let interger1 : number = 0
+let interger2 : number = 0
 
 // get Numbers
-inerger1 = randint (0, 99)
-inerger2 = randint (0, 99)
+interger1 = randint (0, 99)
+interger2 = randint (0, 99)
  
+ // show number for interger1 
 input.onButtonPressed(Button.A, function() {
-    basic.showNumber(inerger1)
+    basic.showNumber(interger1)
 })
 
+// show number for interger2
 input.onButtonPressed(Button.B, function() {
-    basic.showNumber(inerger2)
+    basic.showNumber(interger2)
 })
 
-input.onGesture(Gesture.Shake, function () 
-{
-    if (inerger1 > inerger2)
-    basic.showString(inerger1 + ">" + inerger2 )
+// if statment
+input.onGesture(Gesture.Shake, function () {
+    if (interger1 > interger2) {
+    basic.showString(interger1 + ">" + interger2 )
 
-    else if (inerger1 < inerger2)
-    basic.showString(inerger1 + "<" + inerger2)
+    } else {
+    basic.showString(interger1 + "<" + interger2)
+    }
 })
